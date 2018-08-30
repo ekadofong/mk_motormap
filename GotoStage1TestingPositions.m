10e1a625cd88e1cce6fb11414fdc0bd956a87fb9
Erin Kado-Fong <kadofong@princeton.edu>
2018-08-30 15:29:20 -0400

    update README

--- GotoStage1TestingPositions.m
+++ GotoStage1TestingPositions.m
@@ -4,7 +4,7 @@
 
 % === Python set-up
 
-pyversion ( '/Users/kadofong/env/bin/python' ) % If your preferred python
+%pyversion ( '/Users/kadofong/env/bin/python' ) % If your preferred python
 % installation is not the *system* python installation, redirect here.
 %   Note: The Matlab path is not the same as $PATH, so make sure that this
 %   is set if you usually use e.g. a virtualenvironment
