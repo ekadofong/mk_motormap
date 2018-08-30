a54b1438452d5057c86d4b48657f7d7877bf8a0c
Erin Kado-Fong <kadofong@princeton.edu>
2018-08-30 14:44:09 -0400

    add up packaging files

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="mk_motormap",
+    version='v0.1',
+    author="Erin Kado-Fong",
+    author_email="kadofong@princeton.edu",
+    packages=["modelcobras"],
+    url="https://github.com/ekadofong/mk_motormap",
+    license="MIT",
+    description="Generate controlled step motor maps",
+    install_requires=['scipy','astropy','george','pandas',
+                      'matplotlib','numpy']    
+)
