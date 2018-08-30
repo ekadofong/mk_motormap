162cd3e4001443372b4f49dc6689094921e3a628
Erin Kado-Fong <kadofong@princeton.edu>
2018-08-30 14:14:48 -0400

    add matlab wrapper

---
+++ matlabwrapper_mm.m
@@ -0,0 +1,13 @@
+% Matlab wrapper for python scripts to generate controlled step
+% motor maps.
+
+
+% === Python set-up
+
+% pyversion ( '/Users/kadofong/env/bin/python' ) % If your preferred python
+% installation is not the *system* python installation, redirect here.
+%   Note: The Matlab path is not the same as $PATH, so make sure that this
+%   is set if you usually use e.g. a virtualenvironment
+
+py.importlib.import_module ( 'mk_motormap' )
+py.mk_motormap.main ()
